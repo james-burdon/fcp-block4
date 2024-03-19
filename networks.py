528195349d6080c1041fd66d3d0ebef96714c1c9
james-burton-0 <ky23212@bristol.ac.uk>
2024-03-19 13:31:52 +0000

    updated networks.py

--- networks.py
+++ networks.py
@@ -32,3 +32,28 @@ class BinarySearchTree:
             print(" " * indent + str(self.value))
         if self.left:
             self.left.pretty_print(indent + 4)
+
+def test_search():
+    #Values to store in the binary search tree
+    values_to_add = [5, 8, 2, 4, 6, 7, 3]
+
+    #Create a root node with the first element of the list
+    root = BinarySearchTree(values_to_add[0])
+
+    #Add remaining elements to the tree
+    for val in values_to_add[1:]:
+        root.insert(val)
+
+    print("Running tests")
+    assert(root.search(5)==True), "Test 1"
+    assert(root.search(8)==True), "Test 2"
+    assert(root.search(2)==True), "Test 3"
+    assert(root.search(4)==True), "Test 4"
+    assert(root.search(6)==True), "Test 5"
+    assert(root.search(7)==True), "Test 6"
+    assert(root.search(3)==True), "Test 7"
+    assert(root.search(10)==False) , "Test 8"
+    print("Tests finished")
+
+if __name__ == "__main__":
+    test_search()

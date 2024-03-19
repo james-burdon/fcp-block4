5217ae3e2146185704a9f9d216c6695f0aca310c
james-burton-0 <ky23212@bristol.ac.uk>
2024-03-19 14:44:45 +0000

    updated networks.py on 19th march 2024 at 14:44

--- networks.py
+++ networks.py
@@ -1,73 +1,145 @@
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, item):
+        self.queue.append(item)
+
+    def pop(self, place):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(place)
+
+    def is_empty(self):
+        return len(self.queue) == 0
 
 
-class BinarySearchTree:
+class Node:
+    '''
+    Class to represent a node in an undirected graph
+    Each node has a floating point value and some neighbours
+    Neighbours is a numpy array representing the row of the adjacency matrix that corresponds to the node
+    '''
 
-    def __init__(self, value):
+    def __init__(self, index, value, connections=[]):
         self.value = value
-        self.left = None
-        self.right = None
+        self.connections = connections
 
-    def insert(self, value):
-        if value < self.value:
-            if self.left is None:
-                self.left = BinarySearchTree(value)
-            else:
-                self.left.insert(value)
-        elif value > self.value:
-            if self.right is None:
-                self.right = BinarySearchTree(value)
-            else:
-                self.right.insert(value)
-        else:
-            print("Error- value is also in Tree")
+    def get_neighbours(self):
+        return np.where(np.array(self.connections) == 1)[0]
 
-    def search(self, value):
-        # Check to see if the current node has the value we are looking for.
-        if self.value == value:
-            return True
 
-        # If value is less than current node's value, search the left sub-tree
-        if value < self.value:
-            # If there's a left child, we need to search it. If not, the value doesn't exist in the tree.
-            if self.left:
-                return self.left.search(value)
-            return False
-        else:
-            # Same process as for the right child.
-            if self.right:
-                return self.right.search(value)
-            return False
+class Graph:
+    def __init__(self, nodes, adjacency_matrix):
+        self.nodes = []
 
-    def pretty_print(self, indent=0):
-        if self.right:
-            self.right.pretty_print(indent + 4)
-            print(" " * indent + str(self.value))
-        if self.left:
-            self.left.pretty_print(indent + 4)
+        for (i, node) in enumerate(nodes):
+            new_node = Node(i, node, adjacency_matrix[i])
+            self.nodes.append(new_node)
 
-def test_search():
-    #Values to store in the binary search tree
-    values_to_add = [5, 8, 2, 4, 6, 7, 3]
+        self.adjacency_matrix = adjacency_matrix
 
-    #Create a root node with the first element of the list
-    root = BinarySearchTree(values_to_add[0])
+    def plot(self, fig=None, ax=None):
+        if fig == None:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_axis_off()
 
-    #Add remaining elements to the tree
-    for val in values_to_add[1:]:
-        root.insert(val)
+        num_nodes = len(self.nodes)
+        network_radius = num_nodes * 10
+        ax.set_xlim([-1.1 * network_radius, 1.1 * network_radius])
+        ax.set_ylim([-1.1 * network_radius, 1.1 * network_radius])
 
-    print("Running tests")
-    assert(root.search(5)==True), "Test 1"
-    assert(root.search(8)==True), "Test 2"
-    assert(root.search(2)==True), "Test 3"
-    assert(root.search(4)==True), "Test 4"
-    assert(root.search(6)==True), "Test 5"
-    assert(root.search(7)==True), "Test 6"
-    assert(root.search(3)==True), "Test 7"
-    assert(root.search(10)==False) , "Test 8"
-    print("Tests finished")
+        for (i, node) in enumerate(self.nodes):
+            node_angle = i * 2 * np.pi / num_nodes
+            node_x = network_radius * np.cos(node_angle)
+            node_y = network_radius * np.sin(node_angle)
 
-if __name__ == "__main__":
-    test_search()
+            # to allow the string values for nodes
+            # circle = plt.Circle((node_x, node_y), network_radius / 10, color=cm.hot(node.value), fill=False)
+            circle = plt.Circle((node_x, node_y), network_radius / 10, fill=False)
+            ax.add_patch(circle)
+            ax.text(node_x * 1.05, node_y * 1.05, node.value)
+
+            for neighbour_index in range(i + 1, num_nodes):
+                if node.connections[neighbour_index]:
+                    neighbour_angle = neighbour_index * 2 * np.pi / num_nodes
+                    neighbour_x = network_radius * np.cos(neighbour_angle)
+                    neighbour_y = network_radius * np.sin(neighbour_angle)
+                    ax.plot((node_x, neighbour_x), (node_y, neighbour_y), color='black')
+        plt.show()
+
+
+nodes = [5, 2, 4, 1]
+connectivity = np.array([[0, 0, 1, 1],
+                         [0, 0, 1, 1],
+                         [1, 1, 0, 0],
+                         [1, 1, 0, 0]])
+graph = Graph(nodes, connectivity)
+graph.plot()
+
+
+def graph_stuff():
+    nodes = ['Cotham', 'Cabot', 'Clifton East', 'Clifton', 'Stoke Bishop', 'Westbury', 'Henleaze', 'Bishopston',
+             'Redland']
+    # nodes = [1,2,3,4,5,6,7,8,9]
+    connectivity = np.array([[0, 1, 1, 0, 0, 0, 0, 0, 1],
+                             [1, 0, 1, 1, 0, 0, 0, 0, 0],
+                             [1, 1, 0, 1, 1, 0, 0, 0, 0],
+                             [0, 1, 1, 0, 1, 0, 0, 0, 0],
+                             [0, 0, 1, 1, 0, 1, 1, 0, 1],
+                             [0, 0, 0, 0, 1, 0, 1, 0, 1],
+                             [0, 0, 0, 0, 1, 1, 0, 1, 1],
+                             [0, 0, 0, 0, 0, 0, 1, 0, 1],
+                             [1, 0, 0, 0, 1, 1, 1, 1, 0]])
+    graph = Graph(nodes, connectivity)
+    # graph.legend(['Cotham', 'Cabot', 'Clifton East', 'Clifton', 'Stoke Bishop', 'Westbury', 'Henleaze', 'Bishopston', 'Redland'])
+    graph.plot()
+
+    start_node = graph.nodes[0]
+    goal = graph.nodes[-1]
+    search_queue = Queue()
+    search_queue.push(start_node)
+    visited = []
+
+    while not search_queue.is_empty():
+        node_to_check = search_queue.pop(0)
+
+        if node_to_check == goal:
+            node_to_check = goal
+            start_node.parent = None
+            route = []
+
+            while node_to_check.parent:
+                route.append(node_to_check)
+                node_to_check = node_to_check.parent
+            route.append(node_to_check)
+
+            print([node.value for node in route[::-1]])
+
+        for neighbour_index in node_to_check.get_neighbours():
+            neighbour = graph.nodes[neighbour_index]
+
+            if neighbour_index not in visited:
+                search_queue.push(neighbour)
+                visited.append(neighbour_index)
+                neighbour.parent = node_to_check
+
+
+
+def bfs():
+    # start_node = graph.nodes[0]
+    start_node = start
+    goal = graph.nodes[-1]
+    search_queue = Queue()
+    search_queue.push(start_node)
+    visited = []
+    return
+
+
+graph_stuff()
